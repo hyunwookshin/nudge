39a7e60275de8a810175a16f62ac13cfd67c7f84
Hyunwoook Shin <hyunwook.james.shin@gmail.com>
2024-07-27 19:16:21 +0000

    models: add read time in reminder

--- models/reminder.py
+++ models/reminder.py
@@ -11,12 +11,17 @@ class Reminder():
         self.priority = info["Priority"]
         self.closed = info["Closed"]
         self.snooze = info.get("Snooze", 0)
+        self.read = self.get_current_utc()
+
+    def get_current_utc(self):
+        return datetime.utcnow().replace(tzinfo=timezone.utc)
 
     def toInfo(self):
         info = {
             "Title": self.title,
             "Description": self.description,
             "Time": self.time.strftime("%Y-%m-%d %H:%M:%S"),
+            "Read": self.read,
             "Link": self.link,
             "Priority": self.priority,
             "Closed": self.closed,
