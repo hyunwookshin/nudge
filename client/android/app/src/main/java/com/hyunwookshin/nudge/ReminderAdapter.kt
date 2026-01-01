bafd2bcfc84bebb672b0e3ed019845c3f81024df
Hyunwook Shin <shincontact@icloud.com>
2025-12-31 18:03:29 -0800

    client: improve UI for reminder cards on android

--- client/android/app/src/main/java/com/hyunwookshin/nudge/ReminderAdapter.kt
+++ client/android/app/src/main/java/com/hyunwookshin/nudge/ReminderAdapter.kt
@@ -54,6 +54,7 @@ class ReminderAdapter : RecyclerView.Adapter<ReminderAdapter.ReminderViewHolder>
     override fun getItemCount(): Int = reminders.size
 
     inner class ReminderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val cardRoot: View = itemView.findViewById(R.id.cardRoot)
         private val title: TextView = itemView.findViewById(R.id.title)
         private val description: TextView = itemView.findViewById(R.id.description)
         private val time: TextView = itemView.findViewById(R.id.time)
@@ -83,7 +84,7 @@ class ReminderAdapter : RecyclerView.Adapter<ReminderAdapter.ReminderViewHolder>
             deleteButton.setOnClickListener {
                 reminderCallback?.onDeleteReminder(reminder)
             }
-            itemView.setBackgroundColor(backgroundColor)
+            cardRoot.background?.mutate()?.setTint(backgroundColor)
         }
     }
 }
